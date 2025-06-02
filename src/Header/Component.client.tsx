1679d29a8a75a93fc3ab7f90996d38df4826180a
Steven Gragg <sgragg5619@gmail.com>
2025-06-02 17:35:52 +0800

    refactor: update environment variables, change ID types to numbers in payload types, add new logo, and enhance header styles

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -30,10 +30,10 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
   }, [headerTheme])
 
   return (
-    <header className="container relative z-20   " {...(theme ? { 'data-theme': theme } : {})}>
-      <div className="py-8 flex justify-between">
+    <header className=" relative z-20 bg-white" {...(theme ? { 'data-theme': theme } : {})}>
+      <div className="py-8 flex justify-between container">
         <Link href="/">
-          <Logo loading="eager" priority="high" className="invert dark:invert-0" />
+          <Logo loading="eager" priority="high" className="" />
         </Link>
         <HeaderNav data={data} />
       </div>
--- src/Header/Nav/index.tsx
+++ src/Header/Nav/index.tsx
@@ -12,13 +12,13 @@ export const HeaderNav: React.FC<{ data: HeaderType }> = ({ data }) => {
   const navItems = data?.navItems || []
 
   return (
-    <nav className="flex gap-3 items-center">
+    <nav className="flex gap-3 items-center text-zinc-900">
       {navItems.map(({ link }, i) => {
-        return <CMSLink key={i} {...link} appearance="link" />
+        return <CMSLink key={i} {...link} appearance="link" className="text-zinc-900" />
       })}
       <Link href="/search">
         <span className="sr-only">Search</span>
-        <SearchIcon className="w-5 text-primary" />
+        <SearchIcon className="w-5 text-zinc-900" />
       </Link>
     </nav>
   )
